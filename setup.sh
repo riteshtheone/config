e1370f804320b85fd93b5905928a9e997783dbff
Ritesh Kumar <riteshnagwan@gmail.com>
2026-01-21 13:45:56 +0530

    update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 termux-change-repo
-pkg update && yes N | pkg upgrade -y
+pkg update && yes | pkg upgrade -y
 
 curl -LO "https://raw.githubusercontent.com/riteshtheone/config/refs/heads/main/dist/setup"
 
